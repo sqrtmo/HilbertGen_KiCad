9159dba58e1ad38680808ac376ffb077eb46945e
sq <sqrtmo@gmail.com>
2022-07-23 16:00:14 +0300

    added info to readme, icon file

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # HilbertGen_KiCad
 Hilbert curve generator plugin for KiCad
+
+The plugin aims to make the PCB heaters easier and faster to develop.
+At this point it is able to make only square shaped patterns. 
+Code is based on: https://www.youtube.com/watch?v=dSK-MW-zuAc
+
+# Warning!
+Current KiCad version 6.0.6
+This plugin is in working stage and is not fully tested!
+
+# Installation
+MacOS - download, unzip and copy the folder to /Users/your name/Documents/KiCad/6.0/scripting/plugins
+
+# Error handling
+The startup error handler is taken from: https://github.com/MitjaNemec/Kicad_action_plugins/blob/master/replicate_layout/__init__.py
+Startup script will generate a log file containing the error message in the plugin source folder.
+
+# todo
+Add pattern edge fillets
+
--- hilbertGen.py
+++ hilbertGen.py
@@ -3,47 +3,9 @@
 
 import pcbnew
 import wx
+import os
 
 from . import gui
-# from . import hilbert
-
-# track_widht = 0.3
-
-# hilb_order = 5
-# hilb_N = 2**hilb_order
-# hilb_edges = hilb_N*hilb_N
-
-# hilb_size = 100.0 # square size
-# segment_len = hilb_size / hilb_N
-# whole_len = segment_len*(hilb_edges-1)
-# track_thickness = 0.0347 # 1 oz
-# Tamb = 25
-
-# resistance = (1.7e-5 * whole_len / (track_thickness * track_widht)) * (1 + 3.9e-3 * (Tamb-25)) # https://www.omnicalculator.com/other/pcb-trace-resistance
-
-# fillet = 0.5
-
-# hilb_segment_len = hilb_size[0] / hilb_N
-
-
-
-
-    # fillet = pcbnew.PCB_ARC(board)
-    # fillet.SetStart( pcbpoint( (p_start[0], p_start[1]) ) )
-    # fillet.SetMid  ( pcbpoint( (p_start[0]-p_end[0], p_start[1]-p_end[1]) ) )
-    # fillet.SetEnd  ( pcbpoint( (p_end[0]  , p_end[1]  ) ) )
-
-    # fillet.SetLayer(pcbnew.F_Cu)
-    # fillet.SetWidth(int(track_widht * 1e6))
-    # group.AddItem(fillet)
-    # board.Add(fillet)
-
-# def InitSimpleGui(board):
-#   sg = gui.SimpleGui(None, board)
-#   sg.Show(True)
-#   return sg
-
-
 
 class patternGen(pcbnew.ActionPlugin):
     """Draw hilbert curve"""
@@ -52,7 +14,7 @@ class patternGen(pcbnew.ActionPlugin):
         self.category = "Modify PCB"
         self.description = "generates a hilber curve pattern"
         self.show_toolbar_button = True
-        # self.icon_file_name = os.path.join(os.path.dirname(__file__), 'simple_plugin.png')
+        self.icon_file_name = os.path.join(os.path.dirname(__file__), 'h_ico.png')
 
     def Run( self ):
         board = pcbnew.GetBoard()
